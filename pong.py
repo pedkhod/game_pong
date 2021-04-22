280c824caeb6ff1be07f72c107f69d0ac30c5661
pedkhod <pkhoddami@gmail.com>
2021-04-22 15:29:24 +0430

    shap

--- pong.py
+++ pong.py
@@ -11,17 +11,37 @@ wn.tracer(0)
 
 #paddle a
 
-paddle_a = turtle.Turtle
+paddle_a = turtle.Turtle()
 paddle_a.speed(0)
-paddle_a.shape=('square')
+paddle_a.shape("square")
 paddle_a.color('white')
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350,0)
 #paddle b
 
-
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color('white')
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
 #ball
-
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color('white')
+ball.penup()
+ball.goto(0,0)
+# function
+def paddle_a_up():
+    y=paddle_a.ycor()
+    y+=20
+    paddle_a.sety(y)
+#keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
 
 
 
